7f34e2cfd1c1eaa00158a4da458ae36836b318a9
imasha-rupasinghe <ar112883@fhss.sjp.ac.lk>
2025-08-03 19:09:02 +0530

    Added MVVM Architecture

--- SilkShield_App/SilkShield_App/View/DashbordWindow.xaml.cs
+++ SilkShield_App/SilkShield_App/View/DashbordWindow.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace SilkShield_App
+namespace SilkShield_App.View
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for DashbordWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class DashbordWindow : Window
     {
-        public MainWindow()
+        public DashbordWindow()
         {
             InitializeComponent();
         }
